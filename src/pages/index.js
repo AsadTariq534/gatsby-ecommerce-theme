f6ece1d3ffb2635ab43f9b8b8a0e6334a0126189
AsadTariq534 <109483500+AsadTariq534@users.noreply.github.com>
2022-07-18 02:48:58 +0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,7 +29,7 @@ const IndexPage = () => {
       {/* Hero Container */}
       <Hero
         maxWidth={'500px'}
-        image={'static/Banner01.jpg'}
+        image={'https://github.com/AsadTariq534/gatsby-ecommerce-theme/blob/1bf5e1fee08399c1633781575c13ddcc520333ff/static/Banner01.jpg'}
         title={'Essentials for a cold winter'}
         subtitle={'Discover Autumn Winter 2021'}
         ctaText={'shop now'}
