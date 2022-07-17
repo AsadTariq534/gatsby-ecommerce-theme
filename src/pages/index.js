a59e3eed9296acdfefacef580175a31ce0c7b0dc
AsadTariq534 <109483500+AsadTariq534@users.noreply.github.com>
2022-07-18 02:28:14 +0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,7 +29,7 @@ const IndexPage = () => {
       {/* Hero Container */}
       <Hero
         maxWidth={'500px'}
-        image={'src/assets/ezgif.com-gif-maker (23).png'}
+        image={'/ezgif.com-gif-maker (23).png'}
         title={'Essentials for a cold winter'}
         subtitle={'Discover Autumn Winter 2021'}
         ctaText={'shop now'}
