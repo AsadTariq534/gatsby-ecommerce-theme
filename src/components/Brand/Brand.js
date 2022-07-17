36c8aa6a8f59573a2eb27579b881fbc4f14e724d
AsadTariq534 <109483500+AsadTariq534@users.noreply.github.com>
2022-07-18 03:09:02 +0500

    Update Brand.js

--- src/components/Brand/Brand.js
+++ src/components/Brand/Brand.js
@@ -10,8 +10,8 @@ const Brand = (props) => {
       role={'presentation'}
       onClick={() => navigate('/')}
     >
-      {/* <h4>SYDNEY</h4> */}
-     image = { 'https://github.com/AsadTariq534/gatsby-ecommerce-theme/blob/1acad97f2e7d9754b7ff72fdbf5e855bde064dc0/static/ss.webp'}
+      <h4>Suleman Sweets</h4> 
+     
     </div>
   );
 };
