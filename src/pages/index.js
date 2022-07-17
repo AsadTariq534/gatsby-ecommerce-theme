c5c52b6af7eba32b52490953984918fe7aba0639
AsadTariq534 <109483500+AsadTariq534@users.noreply.github.com>
2022-07-18 04:23:09 +0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,7 +29,7 @@ const IndexPage = () => {
       {/* Hero Container */}
       <Hero
         maxWidth={'300px'}
-        image={'/static/Suleman Sweets-03.jpg'}
+        image={'/SulemanSweetslogo.jpg'}
         title={'Essentials for a cold winter'}
         subtitle={'Discover Autumn Winter 2021'}
         ctaText={'shop now'}
