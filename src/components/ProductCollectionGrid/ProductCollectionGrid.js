2e4161113a44782bc379d287dd0836cbdeea3cfb
AsadTariq534 <109483500+AsadTariq534@users.noreply.github.com>
2022-07-18 04:03:19 +0500

    Update ProductCollectionGrid.js

--- src/components/ProductCollectionGrid/ProductCollectionGrid.js
+++ src/components/ProductCollectionGrid/ProductCollectionGrid.js
@@ -8,25 +8,25 @@ const ProductCollectionGrid = (props) => {
     <div className={styles.root}>
       <ProductCollection
         image={'/collections/collection1.png'}
-        title={'Men'}
+        title={'Sweets / Mithai'}
         text={'SHOP NOW'}
         link={'/shop'}
       />
       <ProductCollection
         image={'/collections/collection2.png'}
-        title={'Women'}
+        title={'Bakery'}
         text={'SHOP NOW'}
         link={'/shop'}
       />
       <ProductCollection
         image={'/collections/collection3.png'}
-        title={'Accessories'}
+        title={'Cakes'}
         text={'SHOP NOW'}
         link={'/shop'}
       />
       <ProductCollection
         image={'/collections/collection4.png'}
-        title={'Simple Cotton'}
+        title={'Fast Food'}
         text={'SHOP NOW'}
         link={'/shop'}
       />
