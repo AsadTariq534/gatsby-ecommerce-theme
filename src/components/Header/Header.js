bbafa1a97361e557230122b151fd58c8e7927840
AsadTariq534 <109483500+AsadTariq534@users.noreply.github.com>
2022-07-21 16:38:45 +0500

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -27,7 +27,7 @@ const Header = (prop) => {
   const [search, setSearch] = useState('');
 
   const searchRef = createRef();
-  const bannerMessage = 'Free shipping';
+  const bannerMessage = 'Website Under Development';
   const searchSuggestions = [
     
   ];
